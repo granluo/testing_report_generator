6a2696ee5042d376d4abe04c2847bb6d1cdcd749
Gran Luo <ollkorrect999@gmail.com>
2020-11-19 14:57:38 -0800

    merge to app.py

--- src/app.py
+++ src/app.py
@@ -5,7 +5,8 @@ from github import Github
 import json_generator
 
 
-repo_client = json_generator.GithubClient(os.getenv("INPUT_TOKEN"), os.getenv("INPUT_REPO"))
+repo_client = json_generator.GithubClient('1d95f4b4d680e937987334ebc0d109d4a39ff038', 'granluo/testing_report_generator')#(os.getenv("INPUT_TOKEN"), os.getenv("INPUT_REPO"))
 json_generator = json_generator.JsonGenerator('testing title', repo_client)
-repo_client. generate_issue(title, json_body, issue_label):
-print(json_generator.generate_json('sample.json'))
+markdown_body, testing_failed = json_generator.generate_markdown()
+repo_client.handle_issue("Title", markdown_body, 'Nightly-testing', testing_failed)
+print(markdown_body)
--- src/json_generator.py
+++ src/json_generator.py
@@ -30,17 +30,16 @@ class GithubClient():
     def get_workflows(self):
        return self.repo.get_workflows()
 
-    def generate_issue(title, json_body, issue_label):
-        markdown, report_failed = create_markdown(json_body)
+    def handle_issue(self, title, markdown_body, issue_label, testing_failed):
         labels = [issue_label]
         issues = self.repo.get_issues(labels=labels, state='open')
         if issues.totalCount == 0:
-            if report_failed:
-                self.repo.create_issue(title=title, body=markdown, labels=[issue_label])
+            if testing_failed:
+                self.repo.create_issue(title=title, body=markdown_body, labels=[issue_label])
         else:
             first_issue = issues[0]
-            if report_failed:
-                first_issue.create_comment(body=markdown)
+            if testing_failed:
+                first_issue.create_comment(body=markdown_body)
             else:
                 first_issue.create_comment(body='All tests passed. Issue closed.')
                 first_issue.edit(state='closed')
@@ -63,13 +62,14 @@ class JsonGenerator():
         workflows = self.client.get_workflows()
         columns = ['Workflow']
         contents = []
+        is_testing_failed = None
         
         for workflow in workflows:
             print(workflow.name)
             if workflow.name in self.skip_tests:
                 print(" workflow {} is skipped.".format(workflow.name))
                 continue
-            runs = workflow.get_runs(event = EVENT)
+            runs = workflow.get_runs()
             # if runs is not empty, then the first run will be get.
             for run in runs:
                 created_time = run.created_at.astimezone(timezone(TIME_ZONE)).strftime("%m/%d")
@@ -77,26 +77,29 @@ class JsonGenerator():
                     columns.append(created_time)
                 run_result = {'Workflow':workflow.name, created_time: "[{}]({})".format(run.conclusion, run.html_url)}
                 if run.conclusion != 'success': 
+                    if is_testing_failed is None:
+                        is_testing_failed = True
                     contents.append(run_result)
                 elif involve_successful_run:
                     contents.append(run_result)
                 break
-        return columns, contents 
+        if is_testing_failed is None:
+            is_testing_failed = False
+        return columns, contents, is_testing_failed
     def create_and_add_table(self, table_name, columns, contents):
        new_table = issue_model.Table(table_name= table_name, column_names = columns, contents=contents)
        self.report.add_table(new_table) 
 
     def generate_json(self, file_name = None):
-       columns, contents = self.get_runs_of_workflows_for_json()
+       columns, contents, is_testing_failed = self.get_runs_of_workflows_for_json()
        self.create_and_add_table("Nightly All Workflow Report", columns, contents)
        if file_name is not None:
            self.report.convert_to_json(file_name)
        output = self.report.convert_to_json()
-       return output
+       return output, is_testing_failed
         
     def generate_markdown(self):
-        json_body = self.generate_json()
-        
+        json_body, is_testing_failed= self.generate_json()
         comment = json.loads(json_body)
 
         text = []
@@ -117,14 +120,15 @@ class JsonGenerator():
                 row.append('')
                 text.append(separator.join(row))
 
-        return ['\n'.join(text), True]
+        return '\n'.join(text), is_testing_failed
 
 
 def main():
-    repo = GithubClient('1d95f4b4d680e937987334ebc0d109d4a39ff038', 'firebase/firebase-ios-sdk')
+    repo = GithubClient('1d95f4b4d680e937987334ebc0d109d4a39ff038', 'granluo/testing_report_generator')
     json_generator = JsonGenerator('testing title', repo)
-    #print(json_generator.generate_json('sample.json'))
-    print(json_generator.generate_markdown())
+    markdown_body, testing_failed = json_generator.generate_markdown()
+    repo.handle_issue("Title", markdown_body, 'Nightly-testing', testing_failed)
+    print(json_generator.generate_json('sample.json'))
     
 if __name__ == "__main__":
     main()
