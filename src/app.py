8bf955638ce3af21059118ffe219798c4273ec41
Gran Luo <ollkorrect999@gmail.com>
2020-11-19 16:33:14 -0800

    Add skip tests.

--- src/app.py
+++ src/app.py
@@ -9,5 +9,5 @@ repo_client = json_generator.GithubClient(os.getenv("INPUT_TOKEN"), os.getenv("I
 #skip_workflows = [] if os.getenv("INPUT_SKIP_WORKFLOWS") is None else os.getenv("INPUT_SKIP_WORKFLOWS").split(",")
 json_generator = json_generator.JsonGenerator("Nightly Testing", repo_client, os.getenv("INPUT_SKIP_WORKFLOWS").split(","))
 markdown_body, testing_failed = json_generator.generate_markdown()
-repo_client.handle_issue(os.getenv("INPUT_TITLE"), markdown_body, os.getenv("INPUT_ISSUE_LABEL"), testing_failed)
+repo_client.handle_issue(os.getenv("INPUT_TITLE"), markdown_body, os.getenv("INPUT_LABEL"), testing_failed)
 print(markdown_body)
