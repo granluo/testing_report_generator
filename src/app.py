cc88883447710ed3d6ef68bf679e055b9b760e9e
Gran Luo <ollkorrect999@gmail.com>
2020-11-20 04:46:43 -0800

    Check targeted repo.

--- src/app.py
+++ src/app.py
@@ -4,7 +4,7 @@ import json
 from github import Github
 import json_generator
 
-
+print(os.getenv())
 repo_client = json_generator.GithubClient(os.getenv("INPUT_TOKEN"), os.getenv("INPUT_HOST_REPO"), os.getenv("INPUT_TARGET_REPO"))
 json_generator = json_generator.JsonGenerator("Nightly Testing", repo_client, os.getenv("INPUT_SKIP_WORKFLOWS").split(","))
 markdown_body, testing_failed = json_generator.generate_markdown(os.getenv("INPUT_JSON_REPORT"))
